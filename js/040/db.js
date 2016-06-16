af2662f2873316180743a4e13922c9c6b6d90957
Smeets <smeets942@web.de>
2016-06-16 13:39:13 +0200

    Erster Entwurf der Datenbank Klasse
    überprüft ob eine Datei vorhanden ist, um dann mit POST / GET zu lesen /
    schreiben

--- js/040/db.js
+++ js/040/db.js
@@ -9,20 +9,35 @@
 //------------------------------------------------------------------------------
 
 var FS = require ('fs');
+var JS = require('jsonfile');
+JS.spaces=4;
 
 //------------------------------------------------------------------------------
 APPLIB.Database_cl = Class.create({
 //------------------------------------------------------------------------------
    initialize: function () {
-
+      // var x=this.execute_px("POST","data/test.json",y);
+      // var x=this.execute_px("GET","data/test.json",null);
    },
    notify_px: function (self_opl, message_spl, data_apl) {
    },
    execute_px: function (method_spl, path_spl, parameters_opl) {
       // hier zur Vereinfachung direkte Auswertung
       var data_o = null;
-      switch (path_spl) {
-
+      try{
+         FS.accessSync(path_spl);
+         
+         switch(method_spl){
+            case 'GET':
+                  data_o = JS.readFileSync(path_spl);
+                  break;
+            case 'POST':
+                  JS.writeFileSync(path_spl, parameters_opl);
+                  break;
+         }
+      }
+      catch(e){
+         console.error("Die Datei ist nicht vorhanden!");
       }
       return data_o;
    }
