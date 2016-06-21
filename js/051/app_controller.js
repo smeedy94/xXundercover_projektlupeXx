d9e8fc304d7e30744c784dae39e07c6abe0c8817
Smeets <smeets942@web.de>
2016-06-21 22:01:20 +0200

    Class aufgeteil in verschieden files

---
+++ js/051/app_controller.js
@@ -0,0 +1,72 @@
+APP.appController_cl = Class.create({
+//------------------------------------------------------------------------------
+   initialize: function () {
+
+   		this.view_o = new APP.appUI_cl();
+
+   		this.zoomState = 0;
+   		this.zoom_o = null
+
+         this.canvas = null;
+
+         APP.es_o.subscribe_px(this, 'app_cont');
+
+
+   },
+   notify_px: function (self_opl, message_spl, data_apl) {
+      console.log(data_apl);
+      switch(data_apl[0]){
+         case 'in':
+            this.succ();
+            break;
+         case 'out':
+            this.prev();
+            break;
+      }
+   },
+   canClose_px: function () {
+      return true;
+   },
+   close_px: function () {
+   },
+   createEventHandler_p: function () {
+    /* Ereignisbehandlung einrichten */
+   this.zoom_o[this.zoomState].createEventHandler_p();
+
+   },
+   destroyEventHandler_p:function(){
+   this.zoom_o[this.zoomState].destroyEventHandler_p();
+
+   },
+   render_px: function (data_opl) {
+     this.canvas = oCanvas.create({
+            canvas: "#canvas",
+            background: "#A2B5CD",
+            fps: 60
+      });
+      this.zoom_o = [new APP.zoom0_cl(this.canvas),new APP.zoom1_cl(this.canvas) ];
+
+
+
+
+   	this.view_o.render_px();
+		this.zoom_o[this.zoomState].render_px();
+      // this.createEventHandler_p();
+
+   },
+   succ: function(){
+         var id = this.zoom_o[this.zoomState].akt_o.id_s;
+         this.zoom_o[this.zoomState].close_px();
+   		this.zoomState++;
+         this.zoom_o[this.zoomState].render_px(id);
+
+   },
+   prev: function(){
+         this.zoom_o[this.zoomState].close_px();
+   		this.zoomState--;
+         this.zoom_o[this.zoomState].render_px();
+
+  }
+ 
+
+});
--- js/051/zoom1.js
+++ js/051/zoom1.js
@@ -1,17 +1,15 @@
-
-
-APP.zoom0_cl = Class.create({
+APP.zoom1_cl = Class.create({
 //------------------------------------------------------------------------------
    initialize: function (can) {
-         this.model = new APP.zoom0_mpde_cl();
+         this.model = new APP.zoom1_mpde_cl();
          this.akt_o = null;
          this.canvas = can;
-         this.scene = can.scenes.create("zoom0", function(){});
+         this.scene = can.scenes.create("zoom1", function(){});
 
          this.loadBox();
 
 
-         APP.es_o.subscribe_px(this, 'zoom0');
+         APP.es_o.subscribe_px(this, 'zoom1');
 
 
     },
@@ -41,14 +39,13 @@ APP.zoom0_cl = Class.create({
       return true;
    },
    close_px: function () {
-      this.canvas.scenes.unload("zoom0");
+      this.canvas.scenes.unload("zoom1");
       this.destroyEventHandler_p();
+
    },
    render_px: function (data_opl) {
-      $("#zoomStateText").html("Zoom 0");
-      
-      
-      this.canvas.scenes.load("zoom0");
+      $("#zoomStateText").html("Zoom 1");
+      this.canvas.scenes.load("zoom1");
       this.createEventHandler_p();
    },
    loadBox:function(){
@@ -58,7 +55,7 @@ APP.zoom0_cl = Class.create({
          var box = this.canvas.display.rectangle(data[x]);
          var text = this.canvas.display.text( data[x].text_c );
          
-         that = this;
+         that2 = this;
 
          box.dragAndDrop({
          start: this.select,
@@ -71,17 +68,16 @@ APP.zoom0_cl = Class.create({
       }
    },
    select:function(){
-      console.log(that);
          this.fadeTo(1,"short","ease-in-out-cubic", function(){})
-         if(that.akt_o != null && that.akt_o != this)
-            that.akt_o.fadeTo(0.55,"short","ease-in-out-cubic", function(){})
-         that.akt_o = this;
+         if(that2.akt_o != null && that2.akt_o != this)
+            that2.akt_o.fadeTo(0.55,"short","ease-in-out-cubic", function(){})
+         that2.akt_o = this;
 
          APP.es_o.publish_px('ui', ['en']);
    },
 
    delete: function(){
-      this.canvas.removeChild(this.akt_o);
+      this.scene.remove(this.akt_o);
 
       this.model.deleteBox(this.akt_o.id_s);
 
@@ -93,11 +89,9 @@ APP.zoom0_cl = Class.create({
    },
 
    addBox: function(data_apl){
-      var next_id = APP.db_o.getNextId("data/projekte.json");
+      var next_id = APP.db_o.getNextId("data/personal.json");
       var name=data_apl[0]['value'];
-      var auftraggeber=data_apl[1]['value'];
-      var frist=data_apl[2]['value'];
-      var kosten=data_apl[3]['value'];
+ 
 
       
       var conf= {
@@ -109,9 +103,7 @@ APP.zoom0_cl = Class.create({
           opacity: 0.55,
           id_s:next_id,
           name:name,
-          auftraggeber:auftraggeber,
-          frist:frist,
-          kosten:kosten,
+
 
           text_c: {
             x: 25,
@@ -127,7 +119,7 @@ APP.zoom0_cl = Class.create({
       var text = this.canvas.display.text( conf.text_c );
 
 
-      that = this;
+      that2 = this;
       box.dragAndDrop({
          start: this.select,
          end: this.updateBoxPos
@@ -141,7 +133,7 @@ APP.zoom0_cl = Class.create({
    },
    updateBoxPos: function(){
       var id = this.id; 
-      that.model.updateBoxPos(this.id_s, this.x, this.y);
+      that2.model.updateBoxPos(this.id_s, this.x, this.y);
    },
    updateBox:function(conf){
       // console.log(conf);
@@ -155,13 +147,13 @@ APP.zoom0_cl = Class.create({
 
    },
    openModel: function(case_s){
-      var modal = UIkit.modal("#add_projekt_modal");
+      var modal = UIkit.modal("#add_personal_modal");
       if(case_s=='add'){
-         $("#add_projekt_modal .uk-modal-header").html('Projekt erstellen');
+         $("#add_personal_modal .uk-modal-header").html('Personal erstellen');
          $("#editbtninform").hide();
          $("#addbtninform").show();
       }else{
-         $("#add_projekt_modal .uk-modal-header").html('Projekt bearbeiten');
+         $("#add_personal_modal .uk-modal-header").html('Personal bearbeiten');
          $("#editbtninform").show();
          $("#addbtninform").hide();
       }
@@ -171,20 +163,20 @@ APP.zoom0_cl = Class.create({
              modal.show();
       }
     },
-   onClick: function(event_opl){
+  onClick: function(event_opl){
          var action = $(event_opl.target).attr("data-action");
             event_opl.preventDefault();
 
       switch(action){
         case 'add':
-          APP.es_o.publish_px('zoom0', ['add',null]);
+          APP.es_o.publish_px('zoom1', ['add',null]);
           break;
         case 'edit':
-            var data = $("#add_projekt_modal form").serializeArray();
-            APP.es_o.publish_px('zoom0', ['edit',data]);
+            var data = $("#add_personal_modal form").serializeArray();
+            APP.es_o.publish_px('zoom1', ['edit',data]);
           break;
         case 'delete':
-          APP.es_o.publish_px('zoom0', ['delete']);
+          APP.es_o.publish_px('zoom1', ['delete']);
           break;
         case 'out':
           APP.es_o.publish_px('app_cont', ['out']);
@@ -193,30 +185,27 @@ APP.zoom0_cl = Class.create({
         case 'in':
           APP.es_o.publish_px('app_cont', ['in']);
           break;
+
         case 'close':
           window.close();
           break;
          case 'save':
-            var data = $("#add_projekt_modal form").serializeArray();
-            APP.es_o.publish_px('zoom0', ['save',data]);
+            var data = $("#add_personal_modal form").serializeArray();
+            APP.es_o.publish_px('zoom1', ['save',data]);
             break;
          case 'update':
-            var data = $("#add_projekt_modal form").serializeArray();
-            APP.es_o.publish_px('zoom0', ['update',data]);
+            var data = $("#add_personal_modal form").serializeArray();
+            APP.es_o.publish_px('zoom1', ['update',data]);
             break;
       }
    },
    createEventHandler_p:function(){
       $("#header button").on('click', this.onClick.bind(this));
-      $("#add_projekt_modal").on('click','button', this.onClick.bind(this));
+      $("#add_personal_modal").on('click','button', this.onClick.bind(this));
    },
    destroyEventHandler_p:function(){
       $("#header button").off();
-      $("#add_projekt_modal").off();
+      $("#add_personal_modal").off();
    }
 
 });
-
-
-
-
